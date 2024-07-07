a1c154f3447f92764381e64a5638ffd538296261
Vinskao <tianyikao@gmail.com>
2024-07-07 16:11:44 +0800

    info: v0.1 completed login oauth2 infrastucture

--- src/main/java/com/distributed_system_design_lab/resource_server/controller/ArticlesController.java
+++ src/main/java/com/distributed_system_design_lab/resource_server/controller/ArticlesController.java
@@ -3,8 +3,19 @@ package com.distributed_system_design_lab.resource_server.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 控制器類，用於處理 /articles 請求並返回文章數據。
+ * 
+ * @author vinskao
+ * @version 0.1
+ */
 @RestController
 public class ArticlesController {
+    /**
+     * 處理 GET /articles 請求，返回文章列表。
+     *
+     * @return 文章標題的字符串數組
+     */
     @GetMapping("/articles")
     public String[] getArticles() {
         return new String[] { "Article 1", "Article 2", "Article 3" };
